a9dbbaafacffb91f46171550dc9642e8565c36b4
Salmaan <salmaan.arif.ali@gmail.com>
2021-01-13 01:18:50 -0500

    commit tree a824eaf2d3fccbd7cf79edd411ab07082d355801

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
   #your code here
+
+  def initialize(attr_hash)
+    attr_hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=",value)
+    end
+  end
 end
